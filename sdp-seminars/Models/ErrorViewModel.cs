4d9d8663c82cfa16c9d434a4e9b7c5a8c225a314
Sean <sean_mysell@hotmail.com>
2018-08-07 23:16:56 +1000

    Add project files.

---
+++ sdp-seminars/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace sdp_seminars.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ sdp-seminars/Services/IEmailSender.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace sdp_seminars.Services
+{
+    public interface IEmailSender
+    {
+        Task SendEmailAsync(string email, string subject, string message);
+    }
+}
